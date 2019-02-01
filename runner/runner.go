0d86283b12440b8e27f0059ce6f6f680e2ac2c49
x1unix <denis0051@gmail.com>
2019-02-02 01:11:02 +0200

    runner: check if task should be ran

--- runner/runner.go
+++ runner/runner.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/x1unix/gilbert/manifest"
 	"github.com/x1unix/gilbert/scope"
+	"github.com/x1unix/gilbert/tools/shell"
+	"strings"
 
 	"github.com/x1unix/gilbert/logging"
 	"github.com/x1unix/gilbert/plugins"
@@ -32,6 +34,12 @@ func (t *TaskRunner) RunJob(job *manifest.Job) error {
 	ctx := scope.CreateContext(t.CurrentDirectory, job.Vars).
 		AppendGlobals(t.Manifest.Vars)
 
+	// check if job should be run
+	if !t.shouldRunJob(job, ctx) {
+		t.Log.SubLogger().Info("Step was skipped")
+		return nil
+	}
+
 	if job.InvokesPlugin() {
 		factory, err := t.PluginByName(job.Plugin)
 		if err != nil {
@@ -46,6 +54,35 @@ func (t *TaskRunner) RunJob(job *manifest.Job) error {
 	}
 
 	return fmt.Errorf("nested task invocation support is not supported, please use plugins for jobs")
+}
+
+func (t *TaskRunner) shouldRunJob(job *manifest.Job, ctx *scope.Context) bool {
+	condCmd := strings.TrimSpace(job.Condition)
+	if condCmd == "" {
+		return true
+	}
+
+	l := t.Log.SubLogger()
+	condCmd, err := ctx.ExpandVariables(condCmd)
+	if err != nil {
+		l.Error(err.Error())
+		l.Warn("Failed to parse value inside 'if' expression, job will be skipped")
+		return false
+	}
+	cmd := shell.PrepareCommand(condCmd)
+
+	l.Debug("assert command: '%s'", condCmd)
+
+	// Return false if command failed to start or returned bad exit code
+	if err := cmd.Start(); err != nil {
+		return false
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return false
+	}
+
+	return true
 }
 
 func (t *TaskRunner) TaskByName(taskName string) (taskPtr *manifest.Task, ok bool) {
