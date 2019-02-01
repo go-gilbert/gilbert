c947345194d03df0c89c11a7afbe9548e7820bdd
x1unix <denis0051@gmail.com>
2019-02-02 00:40:25 +0200

    scope: add docs

--- scope/context.go
+++ scope/context.go
@@ -5,17 +5,20 @@ import (
 	"path/filepath"
 )
 
+// Vars is a set of declared variables
 type Vars map[string]string
 
+// Context contains a set of globals and variables related to specific job
 type Context struct {
-	Globals     Vars
-	Variables   Vars
+	Globals     Vars // Globals is set of global variables for all tasks
+	Variables   Vars // Variables is set of variables for specific job
 	processor   ExpressionProcessor
 	Environment struct {
 		ProjectDirectory string
 	}
 }
 
+// CreateContext creates a new context
 func CreateContext(projectDirectory string, vars Vars) (c *Context) {
 	c = &Context{
 		Globals: Vars{
@@ -31,6 +34,7 @@ func CreateContext(projectDirectory string, vars Vars) (c *Context) {
 	return
 }
 
+// AppendGlobals appends global variables to the context
 func (c *Context) AppendGlobals(globals Vars) *Context {
 	for k, v := range globals {
 		c.Globals[k] = v
@@ -39,11 +43,13 @@ func (c *Context) AppendGlobals(globals Vars) *Context {
 	return c
 }
 
+// Global returns a global variable value by it's name
 func (c *Context) Global(varName string) (out string, ok bool) {
 	out, ok = c.Globals[varName]
 	return
 }
 
+// Var returns a local variable value by it's name
 func (c *Context) Var(varName string) (isLocal bool, out string, ok bool) {
 	out, ok = c.Variables[varName]
 	if ok {
@@ -57,6 +63,7 @@ func (c *Context) Var(varName string) (isLocal bool, out string, ok bool) {
 	return
 }
 
+// ExpandVariable expands an expression stored inside a passed string
 func (c *Context) ExpandVariables(str string) (out string, err error) {
 	return c.processor.ReadString(str)
 }
