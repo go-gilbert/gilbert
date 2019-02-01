62b5b899b7a587a151d2d58e21737ed8a4b256e2
x1unix <denis0051@gmail.com>
2019-02-01 23:03:33 +0200

    shell: move env to shared package

---
+++ tools/shell/env.go
@@ -0,0 +1,22 @@
+package shell
+
+// Environment is map of predefined scope variables
+type Environment map[string]string
+
+// Empty checks if predefined vars list is empty
+func (e Environment) Empty() bool {
+	return e == nil || (len(e) == 0)
+}
+
+func (e Environment) ToArray(defaults ...string) (arr []string) {
+	arr = make([]string, 0, len(defaults)+len(e))
+	for k, v := range e {
+		arr = append(arr, k+"="+v)
+	}
+
+	if len(defaults) == 0 {
+		return arr
+	}
+
+	return append(arr, defaults...)
+}
