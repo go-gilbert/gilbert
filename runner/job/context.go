f6e81a188991f9a632dce40d703f6b816f326af3
x1unix <denis0051@gmail.com>
2019-03-07 17:02:22 +0200

    context: commit result once only

--- runner/job/context.go
+++ runner/job/context.go
@@ -2,10 +2,11 @@ package job
 
 import (
 	"context"
-	"github.com/x1unix/gilbert/logging"
-	"github.com/x1unix/gilbert/scope"
 	"sync"
 	"time"
+
+	"github.com/x1unix/gilbert/logging"
+	"github.com/x1unix/gilbert/scope"
 )
 
 type RunContext struct {
@@ -14,7 +15,7 @@ type RunContext struct {
 	Context  context.Context
 	Error    chan error
 	wg       *sync.WaitGroup
-	finished bool
+	once     sync.Once
 }
 
 func (r *RunContext) SetWaitGroup(wg *sync.WaitGroup) {
@@ -26,6 +27,8 @@ func (r *RunContext) ChildContext() RunContext {
 		RootVars: r.RootVars,
 		Logger:   r.Logger.SubLogger(),
 		Context:  r.Context,
+		Error:    r.Error,
+		wg:       r.wg,
 	}
 }
 
@@ -43,17 +46,22 @@ func (r *RunContext) Success() {
 }
 
 func (r *RunContext) Fail(err error) {
-	if r.finished {
-		return
-	}
+	r.once.Do(func() {
+		defer func() {
+			if rec := recover(); rec != nil {
+				r.Logger.Error("Bug: failed to return job result, %v", rec)
+				r.wg.Done()
+			}
+		}()
 
-	r.Error <- err
-	r.finished = true
-	if r.wg != nil {
-		r.wg.Done()
-	}
+		r.Error <- err
+		if r.wg != nil {
+			r.Logger.Debug("ctx: done reported")
+			r.wg.Done()
+		}
+	})
 }
 
 func NewRunContext(rootVars scope.Vars, log logging.Logger, ctx context.Context) RunContext {
-	return RunContext{RootVars: rootVars, Logger: log, Context: ctx, Error: make(chan error)}
+	return RunContext{RootVars: rootVars, Logger: log, Context: ctx, Error: make(chan error, 1)}
 }
