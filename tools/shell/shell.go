879c4212f9980bb226ab0472bd61922b554582f3
x1unix <denis0051@gmail.com>
2019-03-10 06:14:39 +0200

    shell: add KillProcessGroup

--- tools/shell/shell.go
+++ tools/shell/shell.go
@@ -3,8 +3,12 @@ package shell
 import (
 	"fmt"
 	"os/exec"
+	"runtime"
 	"syscall"
 )
+
+// OsWindows is windows os name
+const OsWindows = "windows"
 
 // FormatExitError extracts process wait error and formats it
 func FormatExitError(err error) error {
@@ -24,6 +28,22 @@ func FormatExitError(err error) error {
 }
 
 // PrepareCommand prepares a command to execute
-func PrepareCommand(cmd string) *exec.Cmd {
-	return exec.Command(shellPath, shellCmdPrefix, wrapCommand(cmd))
+func PrepareCommand(cmdName string) *exec.Cmd {
+	cmd := exec.Command(shellPath, shellCmdPrefix, wrapCommand(cmdName))
+
+	// Assign process group (for unix only)
+	if runtime.GOOS != "windows" {
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	}
+
+	return cmd
+}
+
+// KillProcessGroup kills process group created by parent process
+func KillProcessGroup(cmd *exec.Cmd) error {
+	if runtime.GOOS == OsWindows {
+		return cmd.Process.Kill()
+	}
+
+	return syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
 }
