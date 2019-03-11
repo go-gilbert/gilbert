03bf519affc133429eb832b91eacc483d3ff2804
Denis Sedchenko <ascii@live.ru>
2019-03-11 14:43:45 +0200

    shell: add build tags for unix OS'es

---
+++ plugins/builtin/shell/unix.go
@@ -0,0 +1,28 @@
+// +build !windows !js !nacl
+
+package shell
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+const (
+	shellSh         = "/bin/sh"
+	shCommandPrefix = "-c"
+)
+
+func defaultParams() Params {
+	return Params{
+		Shell:          shellSh,
+		ShellExecParam: shCommandPrefix,
+	}
+}
+
+func (p *Params) preparedCommand() string {
+	return p.Command
+}
+
+func decorateCommand(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
