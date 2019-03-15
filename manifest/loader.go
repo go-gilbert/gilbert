f8372825d55f7423d6e78120fddb40481f892512
x1unix <denis0051@gmail.com>
2019-03-15 04:29:27 +0200

    manifest: integrate import loader in manifest reader

--- manifest/loader.go
+++ manifest/loader.go
@@ -14,19 +14,37 @@ func UnmarshalManifest(data []byte) (m *Manifest, err error) {
 	return
 }
 
-// FromDirectory loads gilbert.yaml from specified directory
-func FromDirectory(dir string) (m *Manifest, err error) {
-	location := filepath.Join(dir, FileName)
-	data, err := ioutil.ReadFile(location)
+// LoadManifest loads manifest from specified path and it's imports
+func LoadManifest(path string) (*Manifest, error) {
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("manifest file not found (%s) at %s", FileName, dir)
+		return nil, fmt.Errorf("manifest file not found at '%s'", path)
 	}
 
-	m, err = UnmarshalManifest(data)
+	m, err := UnmarshalManifest(data)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse manifest file:\n  %v", err)
 	}
 
-	m.location = location
-	return m, nil
+	m.location = path
+
+	// Return as-is if no imports declared
+	if len(m.Imports) == 0 {
+		return m, nil
+	}
+
+	// Load imports
+	tree := newImportTree(m)
+	if err := tree.resolveImports(); err != nil {
+		return nil, fmt.Errorf("failed to resolve imports in manifest file - %s", err)
+	}
+
+	result := tree.result()
+	return &result, nil
+}
+
+// FromDirectory loads gilbert.yaml from specified directory
+func FromDirectory(dir string) (m *Manifest, err error) {
+	location := filepath.Join(dir, FileName)
+	return LoadManifest(location)
 }
