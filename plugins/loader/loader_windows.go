0d9658e0048c00fcf65711030b8a975f845c5b61
x1unix <denis0051@gmail.com>
2019-04-30 05:07:53 +0300

    plugins: fix windows stub

--- plugins/loader/loader_windows.go
+++ plugins/loader/loader_windows.go
@@ -1,5 +1,11 @@
 package loader
 
+import (
+	"errors"
+
+	"github.com/go-gilbert/gilbert-sdk"
+)
+
 // LoadLibrary loads library from provided source
 func LoadLibrary(libPath string) (sdk.PluginFactory, string, error) {
 	return nil, "", errors.New("plugins currently are not supported on Windows")
