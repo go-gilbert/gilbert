993415210a23d5252ed39eb47cd0fde1e62155e3
x1unix <denis0051@gmail.com>
2019-06-10 23:49:48 +0300

    chore: create test log

--- runner/job/context_test.go
+++ runner/job/context_test.go
@@ -3,19 +3,17 @@ package job
 import (
 	"context"
 	"errors"
-	"fmt"
-	sdk "github.com/go-gilbert/gilbert-sdk"
-	"github.com/go-gilbert/gilbert/log"
-	"github.com/stretchr/testify/assert"
-	"io"
-	"os"
 	"sync"
 	"testing"
 	"time"
+
+	sdk "github.com/go-gilbert/gilbert-sdk"
+	"github.com/go-gilbert/gilbert/support/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRunContext_Result(t *testing.T) {
-	rtx := NewRunContext(context.Background(), nil, &testLog{t: t})
+	rtx := NewRunContext(context.Background(), nil, &test.Log{T: t})
 	errs := make(chan error)
 	expected := errors.New("test error")
 	wg := &sync.WaitGroup{}
@@ -33,7 +31,7 @@ func TestRunContext_Result(t *testing.T) {
 }
 
 func TestRunContext_Success(t *testing.T) {
-	rtx := NewRunContext(context.Background(), nil, &testLog{t: t})
+	rtx := NewRunContext(context.Background(), nil, &test.Log{T: t})
 	wg := &sync.WaitGroup{}
 	rtx.SetWaitGroup(wg)
 	go func() {
@@ -49,7 +47,7 @@ func TestRunContext_Success(t *testing.T) {
 
 func TestRunContext_ChildContext(t *testing.T) {
 	vars := sdk.Vars{"foo": "bar"}
-	baseTx := NewRunContext(context.Background(), nil, &testLog{t: t})
+	baseTx := NewRunContext(context.Background(), nil, &test.Log{T: t})
 	baseTx.SetVars(vars)
 	rtx := baseTx.ChildContext().(*RunContext)
 
@@ -62,7 +60,7 @@ func TestRunContext_ChildContext(t *testing.T) {
 
 func TestRunContext_ForkContext(t *testing.T) {
 	vars := sdk.Vars{"foo": "bar"}
-	ctx := NewRunContext(context.Background(), vars, &testLog{t: t})
+	ctx := NewRunContext(context.Background(), vars, &test.Log{T: t})
 	child := ctx.ForkContext().(*RunContext)
 
 	assert.Equal(t, ctx.context, child.context)
@@ -73,7 +71,7 @@ func TestRunContext_ForkContext(t *testing.T) {
 }
 
 func TestRunContext_Cancel(t *testing.T) {
-	rtx := NewRunContext(context.Background(), nil, &testLog{t: t})
+	rtx := NewRunContext(context.Background(), nil, &test.Log{T: t})
 	canceled := false
 	rtx.cancelFn = func() {
 		canceled = true
@@ -89,13 +87,13 @@ func TestRunContext_Cancel(t *testing.T) {
 }
 
 func TestRunContext_Log(t *testing.T) {
-	l := &testLog{t: t}
+	l := &test.Log{T: t}
 	rtx := NewRunContext(context.Background(), nil, l)
 	assert.Equal(t, l, rtx.Log())
 }
 
 func TestRunContext_Timeout(t *testing.T) {
-	rtx := NewRunContext(context.Background(), nil, &testLog{t: t})
+	rtx := NewRunContext(context.Background(), nil, &test.Log{T: t})
 	canceled := false
 	rtx.cancelFn = func() {
 		canceled = true
@@ -112,85 +110,4 @@ func TestRunContext_Context(t *testing.T) {
 	rtx := NewRunContext(context.Background(), nil, nil)
 	assert.NotNil(t, rtx.Context())
 	close(rtx.Errors())
-}
-
-////////////////////////
-//   Test Fixtures    //
-////////////////////////
-
-type testLog struct {
-	t        *testing.T
-	messages []string
-}
-
-func (c *testLog) log(level int, args ...interface{}) {
-	c.t.Log(args...)
-}
-
-func (c *testLog) logf(level int, format string, args ...interface{}) {
-	c.t.Logf(format, args...)
-}
-
-func (c *testLog) SubLogger() sdk.Logger {
-	return c
-}
-
-func (c *testLog) Format(format string, args ...interface{}) string {
-	return fmt.Sprintf(format, args...)
-}
-
-func (c *testLog) Log(args ...interface{}) {
-	c.log(log.LevelMsg, args...)
-}
-
-func (c *testLog) Logf(format string, args ...interface{}) {
-	c.logf(log.LevelMsg, format, args...)
-}
-
-func (c *testLog) Debug(args ...interface{}) {
-	c.log(log.LevelDebug, args...)
-}
-
-func (c *testLog) Debugf(format string, args ...interface{}) {
-	c.logf(log.LevelDebug, format, args...)
-}
-
-func (c *testLog) Warn(args ...interface{}) {
-	c.log(log.LevelWarn, args...)
-}
-
-func (c *testLog) Warnf(format string, args ...interface{}) {
-	c.logf(log.LevelWarn, format, args...)
-}
-
-func (c *testLog) Info(args ...interface{}) {
-	c.log(log.LevelInfo, args...)
-}
-
-func (c *testLog) Infof(format string, args ...interface{}) {
-	c.logf(log.LevelInfo, format, args...)
-}
-
-func (c *testLog) Success(args ...interface{}) {
-	c.log(log.LevelSuccess, args...)
-}
-
-func (c *testLog) Successf(format string, args ...interface{}) {
-	c.logf(log.LevelSuccess, format, args...)
-}
-
-func (c *testLog) Error(args ...interface{}) {
-	c.log(log.LevelError, args...)
-}
-
-func (c *testLog) Errorf(format string, args ...interface{}) {
-	c.logf(log.LevelError, format, args...)
-}
-
-func (c *testLog) Write(data []byte) (int, error) {
-	return fmt.Fprint(os.Stdout, data)
-}
-
-func (c *testLog) ErrorWriter() io.Writer {
-	return os.Stderr
 }
---
+++ support/test/log.go
@@ -0,0 +1,108 @@
+package test
+
+//nolint
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	sdk "github.com/go-gilbert/gilbert-sdk"
+	"github.com/go-gilbert/gilbert/log"
+)
+
+////////////////////////
+//   Test Fixtures    //
+////////////////////////
+
+type Log struct {
+	T        *testing.T
+	messages []string
+}
+
+func (c *Log) log(level int, args ...interface{}) {
+	c.messages = append(c.messages, fmt.Sprint(args...))
+	c.T.Log(args...)
+}
+
+func (c *Log) logf(level int, format string, args ...interface{}) {
+	c.messages = append(c.messages, fmt.Sprintf(format, args...))
+	c.T.Logf(format, args...)
+}
+
+// AssertMessage checks if message has been logged
+func (c *Log) AssertMessage(msg string) {
+	for _, m := range c.messages {
+		if strings.Contains(m, msg) {
+			return
+		}
+	}
+
+	c.T.Errorf("log: expected message '%s' was not logged", msg)
+}
+
+func (c *Log) SubLogger() sdk.Logger {
+	return c
+}
+
+func (c *Log) Format(format string, args ...interface{}) string {
+	return fmt.Sprintf(format, args...)
+}
+
+func (c *Log) Log(args ...interface{}) {
+	c.log(log.LevelMsg, args...)
+}
+
+func (c *Log) Logf(format string, args ...interface{}) {
+	c.logf(log.LevelMsg, format, args...)
+}
+
+func (c *Log) Debug(args ...interface{}) {
+	c.log(log.LevelDebug, args...)
+}
+
+func (c *Log) Debugf(format string, args ...interface{}) {
+	c.logf(log.LevelDebug, format, args...)
+}
+
+func (c *Log) Warn(args ...interface{}) {
+	c.log(log.LevelWarn, args...)
+}
+
+func (c *Log) Warnf(format string, args ...interface{}) {
+	c.logf(log.LevelWarn, format, args...)
+}
+
+func (c *Log) Info(args ...interface{}) {
+	c.log(log.LevelInfo, args...)
+}
+
+func (c *Log) Infof(format string, args ...interface{}) {
+	c.logf(log.LevelInfo, format, args...)
+}
+
+func (c *Log) Success(args ...interface{}) {
+	c.log(log.LevelSuccess, args...)
+}
+
+func (c *Log) Successf(format string, args ...interface{}) {
+	c.logf(log.LevelSuccess, format, args...)
+}
+
+func (c *Log) Error(args ...interface{}) {
+	c.log(log.LevelError, args...)
+}
+
+func (c *Log) Errorf(format string, args ...interface{}) {
+	c.logf(log.LevelError, format, args...)
+}
+
+func (c *Log) Write(data []byte) (int, error) {
+	return fmt.Fprint(os.Stdout, data)
+}
+
+func (c *Log) ErrorWriter() io.Writer {
+	return os.Stderr
+}
