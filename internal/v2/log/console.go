3d239d256201fddb5f3b2dfff2b824ebd4214f2c
x1unix <9203548+x1unix@users.noreply.github.com>
2025-03-08 22:28:59 -0500

    feat: add v2 logger

--- internal/v2/log/console.go
+++ internal/v2/log/console.go
@@ -1,1 +1,76 @@
 package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+var (
+	errColor     = color.New(color.FgRed)
+	warnColor    = color.New(color.FgYellow)
+	successColor = color.New(color.FgGreen)
+)
+
+type ConsoleWriter struct {
+	// NoColor disables ansi colors output.
+	NoColor bool
+}
+
+// NewConsoleWriter returns a new writer that writes log messages in human-friendly format.
+func NewConsoleWriter(noColor bool) ConsoleWriter {
+	return ConsoleWriter{
+		NoColor: noColor,
+	}
+}
+
+func (c ConsoleWriter) writeNoColor(level Level, message string) {
+	dst := writerForLevel(level)
+	_, _ = fmt.Fprintln(dst, message)
+}
+
+func (c ConsoleWriter) Write(level Level, _, message string) {
+	if c.NoColor {
+		c.writeNoColor(level, message)
+		return
+	}
+
+	var (
+		textColor *color.Color
+		prefix    string
+	)
+	switch level {
+	case LevelFatal:
+		prefix = "Fatal error: "
+		textColor = errColor
+	case LevelError:
+		prefix = "Error: "
+		textColor = errColor
+	case LevelWarning:
+		prefix = "Warning: "
+		textColor = warnColor
+	case LevelSuccess:
+		textColor = successColor
+	default:
+		c.writeNoColor(level, message)
+		return
+	}
+
+	dst := writerForLevel(level)
+	if prefix != "" {
+		_, _ = textColor.Fprint(dst, prefix)
+	}
+
+	_, _ = textColor.Fprintln(dst, message)
+}
+
+func writerForLevel(level Level) io.Writer {
+	switch level {
+	case LevelFatal, LevelError, LevelWarning, LevelDebug:
+		return os.Stderr
+	default:
+		return os.Stdout
+	}
+}
--- internal/v2/log/json.go
+++ internal/v2/log/json.go
@@ -1,1 +1,41 @@
 package log
+
+import (
+	"encoding/json"
+	"sync"
+	"time"
+)
+
+const newLine = "\n"
+
+type jsonLine struct {
+	At      time.Time `json:"at"`
+	Level   Level     `json:"level"`
+	Tag     string    `json:"tag,omitempty"`
+	Message string    `json:"msg,omitempty"`
+}
+
+type JSONWriter struct {
+	lock sync.Mutex
+}
+
+// NewJSONWriter returns a writer that writes log messages in JSON format.
+func NewJSONWriter() *JSONWriter {
+	return &JSONWriter{}
+}
+
+func (w *JSONWriter) Write(level Level, tag, message string) {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+
+	dst := writerForLevel(level)
+	line := jsonLine{
+		At:      time.Now(),
+		Level:   level,
+		Tag:     tag,
+		Message: message,
+	}
+
+	_ = json.NewEncoder(dst).Encode(line)
+	_, _ = dst.Write([]byte(newLine))
+}
