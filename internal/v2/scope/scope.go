b1817f11d560043d3a79e47cbe0e25ac6770d3f7
x1unix <9203548+x1unix@users.noreply.github.com>
2025-03-09 22:36:43 -0400

    feat: add scope

--- internal/v2/scope/scope.go
+++ internal/v2/scope/scope.go
@@ -1,1 +1,48 @@
 package scope
+
+type Context uint
+
+const (
+	ContextRoot Context = iota
+	ContextTask
+	ContextMixin
+	ContextJob
+)
+
+const (
+	projectFieldsCount = 2
+	scopeFieldsCount   = 2
+)
+
+const (
+	InputsKey  = "inputs"
+	ProjectKey = "project"
+	EnvKey     = "env"
+)
+
+type Globals struct {
+	Project ProjectInfo
+	Env     map[string]string
+}
+
+type Scope struct {
+	Root    *Scope
+	Parent  *Scope
+	Context Context
+	Globals Globals
+	Inputs  map[string]any
+	Consts  map[string]any
+}
+
+func (s *Scope) Values() map[string]any {
+	count := len(s.Consts) + scopeFieldsCount + projectFieldsCount
+	m := make(map[string]any, count)
+	for k, v := range s.Consts {
+		m[k] = v
+	}
+
+	m[InputsKey] = s.Inputs
+	m[ProjectKey] = s.Globals.Project.Values()
+	m[EnvKey] = s.Globals.Env
+	return m
+}
--- internal/v2/scope/utils.go
+++ internal/v2/scope/utils.go
@@ -1,1 +1,24 @@
 package scope
+
+import (
+	"os"
+	"strings"
+)
+
+// Env returns environment variables in key-value format usable for globals.
+func Env() map[string]string {
+	env := os.Environ()
+	out := make(map[string]string, len(env))
+	for _, str := range env {
+		chunks := strings.SplitN(str, "=", 2)
+		key := chunks[0]
+		if len(chunks) == 1 {
+			out[key] = ""
+			continue
+		}
+
+		out[key] = chunks[1]
+	}
+
+	return out
+}
