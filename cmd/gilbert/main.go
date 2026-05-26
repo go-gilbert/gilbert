d2e0f431ae8f8009c80732b6f16747c84d5705d7
x1unix <9203548+x1unix@users.noreply.github.com>
2026-05-26 00:42:31 -0400

    feat: remove legacy binary package

--- cmd/gilbert/main.go
+++ cmd/gilbert/main.go
@@ -3,21 +3,11 @@ package main
 import (
 	"os"
 
-	"github.com/go-gilbert/gilbert/internal/cmd"
-)
-
-var (
-	// These values will override by linker
-	version = "dev"
-	commit  = "local build"
+	"github.com/go-gilbert/gilbert/internal/v2/cmd"
+	"github.com/go-gilbert/gilbert/internal/v2/log"
 )
 
 func main() {
-	app := cmd.NewCmdRoot(cmd.VersionInfo{
-		Version: version,
-		Commit:  commit,
-	})
-
-	err := app.Run(os.Args)
-	cmd.Exit(err)
+	exitCode := cmd.Main(log.DefaultIOStreams, os.Args)
+	os.Exit(exitCode)
 }
--- testdata/gilbert.sh
+++ testdata/gilbert.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 # go run ../cmd/gilbert2 "$@" --log-level=debug
-go run ../cmd/gilbert2 "$@"
+go run ../cmd/gilbert "$@"
